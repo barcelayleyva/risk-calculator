562d6a0450d444728c61aa279f4db166d7a9e2af
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2024-11-01 08:14:42 -0400

    perf(SEO): 🔍️ update  fetching URL
    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,9 +1,12 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'https://localhost:3000';
   return [
     {
-      url: 'https://risk-calculator-iota.vercel.app',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
